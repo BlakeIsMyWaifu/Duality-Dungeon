6a2ffea0391b56b4dfbdf0fd0ccc3ab659837c40
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2024-01-05 09:15:12 +0000

    📦 Added mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import './styles.css'
+import '@mantine/core/styles.css'
 
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
+	<StrictMode>
 		<App />
-	</React.StrictMode>
+	</StrictMode>
 )
