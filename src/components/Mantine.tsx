94078a39750caddedcd4227c4bf739c2b6061e1e
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2024-01-28 15:38:51 +0000

    💄 Global styles

--- src/components/Mantine.tsx
+++ src/components/Mantine.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css'
+import '~/utils/globalStyles.m.css'
 
 import { type MantineColorsTuple, MantineProvider } from '@mantine/core'
 import { type ReactNode } from 'react'
